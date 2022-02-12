0eb459f66df548369343422f7f2008ebfbd7b1ba
zlewchan <wiktorusyt@protonmail.ch>
2022-02-12 15:02:45 +0100

    miatoll: Streak Bringup
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>
    
    miatoll: this string is not needed in Streak
    
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>
    
    miatoll: I forgot to change one string
    
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_miatoll.mk
+    $(LOCAL_DIR)/streak_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_miatoll-user \
-    spark_miatoll-userdebug \
-    spark_miatoll-eng
+    streak_miatoll-user \
+    streak_miatoll-userdebug \
+    streak_miatoll-eng
---
+++ streak_miatoll.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The The sparkos
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common ProjectStreak stuff.
+$(call inherit-product, vendor/streak/config/common_full_phone.mk)
+
+# ProjectStreak properties
+TARGET_BOOT_ANIMATION_RES := 1080
+STREAK_BUILD_TYPE := OFFICIAL
+
+# Device identifier
+PRODUCT_NAME := streak_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Miatoll
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
