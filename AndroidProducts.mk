c394b34241cce02320931047cecb77a3e38a9559
zlewchan <wiktorusyt@protonmail.ch>
2022-05-21 15:22:01 +0200

    miatoll: Streakify!
    Signed-off-by: zlewchan <wiktorusyt@protonmail.ch>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_miatoll.mk
+    $(LOCAL_DIR)/streak_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_miatoll-user \
-    aosp_miatoll-userdebug \
-    aosp_miatoll-eng
+    streak_miatoll-user \
+    streak_miatoll-userdebug \
+    streak_miatoll-eng
